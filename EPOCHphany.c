f1f5aff31179fe94fbec5e49d6366959f11a500b
Adam Abbas <aabbas@stuy.edu>
2017-09-20 18:15:56 -0400

    Backwards Random Array Time EPOCH other big words... it works!

--- EPOCHphany.c
+++ EPOCHphany.c
@@ -3,32 +3,34 @@
 #include <time.h>
 
 int main(){
-    int forwerd[10];
-    int take_it_back_now_yall[10];
-    int i;
-    int * poeenteer;
+    int forwerd[10]; //intialize the first array
+    int take_it_back_now_yall[10]; //initalize... the second array
+    int i; //One counter variable to rule them all
+    int * poeenteer; //What's the point?
     
-    srand( time(NULL) );
+    srand( time(NULL) ); //Seed that random generator
     
     for(i = 0; i < 9; i++){
-        forwerd[i] = rand();
+        forwerd[i] = rand(); //Populate the first array
     }
-    forwerd[9] = 0;
+    forwerd[9] = 0; //Set the last value to 0!!
     
+        /*
     for(i = 0; i < 10; i++){
-        printf("Arr1[%d]: %d\n", i, forwerd[i]);
+        printf("Arr1[%d]: %d\n", i, forwerd[i]); //Print it out to make sure code happened
+    }
+    */
+        
+    for(i = 9; i >= 0; i--){ //Backwards for loop for a backwards array
+        poeenteer = &forwerd[9-i];   //Go forward first though, and make sure you're getting the pointer
+        take_it_back_now_yall[i] = *poeenteer; //Turn that pointer into a value and assign!!
     }
     
-    for(i = 9; i >= 0; i--){
-        poeenteer = &forwerd[9-i];    
-        take_it_back_now_yall[i] = *poeenteer;
+    //printf("\n\n"); //Make my output pretty 
+    
+   /* for(i = 0; i < 10; i++){
+        printf("Arr2[%d]: %d\n", i, take_it_back_now_yall[i]); #Make sure coding magic is magic
     }
-    
-    printf("\n\n");
-    
-    for(i = 0; i < 10; i++){
-        printf("Arr2[%d]: %d\n", i, take_it_back_now_yall[i]);
-    }
-    
+    */
 
 }
